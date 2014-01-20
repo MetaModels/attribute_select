712fad04794196a27da950385cd038515ce047bb
Andreas Isaak <andy.jared@googlemail.com>
2014-01-20 20:36:40 +0100

    Delete unused comma

--- src/system/modules/metamodelsattribute_select/config/database.sql
+++ src/system/modules/metamodelsattribute_select/config/database.sql
@@ -17,7 +17,7 @@ CREATE TABLE `tl_metamodel_attribute` (
   `select_id` varchar(255) NOT NULL default '',
   `select_alias` varchar(255) NOT NULL default '',
   `select_sorting` varchar(255) NOT NULL default '',
-  `select_where` text NULL,
+  `select_where` text NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- 
@@ -25,5 +25,5 @@ CREATE TABLE `tl_metamodel_attribute` (
 -- 
 
 CREATE TABLE `tl_metamodel_dcasetting` (
-  `select_as_radio` varchar(1) NOT NULL default '0',
+  `select_as_radio` varchar(1) NOT NULL default '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
