1341d4fee40c32ace793b89d55b5a6a5ed69e84c
Mini Model <minimodel@metamodel.me>
2014-05-09 16:14:40 +0200

    Updated translations from transifex.

--- src/system/modules/metamodelsattribute_select/languages/de/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_select/languages/de/tl_metamodel_attribute.php
@@ -8,13 +8,20 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T22:13:06+02:00
+ * last-updated: 2013-07-10T12:10:43+02:00
  */
 
-
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_alias']['0']     = 'Alias-Spalte';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_column']['0']    = 'Werte-Spalte';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_column']['1']    = 'Bitte wählen Sie die Tabellenspalte aus der ein Textwert bezogen werden soll.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_id']['0']        = 'ID-Spalte';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_id']['1']        = 'Bitte wählen Sie die Tabellenspalte aus die als Datensatz-ID dienen solll.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_sorting']['0']   = 'Auswahl-Sortierung';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_sorting']['1']   = 'Bitte wählen Sie einen Eintrag für die Tag-Sortierung aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_table']['0']     = 'Quelltabelle';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_table']['1']     = 'Bitte wählen Sie die Tabelle, aus der die Werte bezogen werden sollen.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_where']['0']     = 'SQL';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_where']['1']     = 'Die Auswahlmöglichkeiten können per SQL eingeschränkt werden.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['sql_error']             = 'Die SQL-Abfrage verursacht einen Fehler.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['select'] = 'Auswahl';
+
