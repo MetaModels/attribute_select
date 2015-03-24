480b217d6e506de8b09dd5c91f8eea3b3fb932e4
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-03-24 15:43:03 +0100

    Author validation.

--- src/MetaModels/Attribute/Select/AbstractSelect.php
+++ src/MetaModels/Attribute/Select/AbstractSelect.php
@@ -6,9 +6,11 @@
  * data in each collection.
  *
  * PHP version 5
+ *
  * @package     MetaModels
  * @subpackage  AttributeSelect
  * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author      Stefan Heimes <stefan_heimes@hotmail.com>
  * @copyright   The MetaModels team.
  * @license     LGPL.
  * @filesource
