f8161c01939f33267d4d0d41b7e3321840017662
Mini Model <minimodel@metamodel.me>
2015-07-13 12:14:25 +0200

    Updated translations from transifex.

--- contao/languages/ru/tl_metamodel_dcasetting.php
+++ contao/languages/ru/tl_metamodel_dcasetting.php
@@ -8,10 +8,12 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2014-12-18T08:13:10+01:00
+ * last-updated: 2015-07-11T08:01:25+02:00
  */
 
-
-$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['0'] = 'Показать как радио-кнопки';
-$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['1'] = 'Выберите этот параметр для изменения типа представления с "выбор" на "радио"';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['0']           = 'Тип отображения';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['1']           = 'Выберите тип нужного отображения.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio_reference']['0'] = 'Показать выбранное меню';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio_reference']['1'] = 'Показать список радио-кнопок';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio_reference']['2'] = 'Показать как всплывающее окно выбора';
 
