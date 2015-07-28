bf38321d215439e9471c3c597ece06ad963e5d8f
Mini Model <minimodel@metamodel.me>
2015-07-28 16:19:23 +0200

    Updated translations from transifex.

--- contao/languages/ru/tl_metamodel_dcasetting.php
+++ contao/languages/ru/tl_metamodel_dcasetting.php
@@ -8,7 +8,7 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-13T12:14:25+02:00
+ * last-updated: 2015-07-13T16:14:05+02:00
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['0']           = 'Тип вывода';
@@ -16,4 +16,8 @@ $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['1']          
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio_reference']['0'] = 'Показать выбранное меню';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio_reference']['1'] = 'Показать список радио-кнопок';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio_reference']['2'] = 'Показать как всплывающее окно выбора';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_maxLevel']['0']           = 'Максимальный уровень в дереве выбора';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_maxLevel']['1']           = 'Если передается значение >0, элемент выше этого уровня не будет выбран.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_minLevel']['0']           = 'Минимальный уровень в дереве выбора';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_minLevel']['1']           = 'Если передается значение >0, элемент ниже этого уровня не будет выбран.';
 
