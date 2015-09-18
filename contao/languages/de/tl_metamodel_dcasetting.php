b823edd90dd19eb1c82ab62e185b8e8f225cdcbb
Mini Model <minimodel@metamodel.me>
2015-09-18 12:14:38 +0200

    Updated translations from transifex.

--- contao/languages/de/tl_metamodel_dcasetting.php
+++ contao/languages/de/tl_metamodel_dcasetting.php
@@ -8,7 +8,7 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-11T04:11:31+02:00
+ * last-updated: 2015-09-18T10:30:49+02:00
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['0']           = 'Anzeigetyp';
@@ -16,5 +16,8 @@ $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['1']          
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio_reference']['0'] = 'Anzeige des Auswahlmenü (Select)';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio_reference']['1'] = 'Anzeige als Liste von Radiobuttons.';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio_reference']['2'] = 'Anzeige als Popup-Picker';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_maxLevel']['0']           = 'Höchste Ebene in der Baumauswahl.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_maxLevel']['1']           = 'Bei einem Wert > 0 können keine Datensätze mit einem niedrigeren Level als dem eingegebenen selektiert werden.';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_minLevel']['0']           = 'Niedrigste Ebene in der Baumauswahl.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_minLevel']['1']           = 'Bei einem Wert > 0 können keine Datensätze mit einem niedrigeren Level als dem eingegebenen selektiert werden.';
 
