e137fa4de4e2093b51a1f989e79fd0b93ee5e10a
Mini Model <minimodel@metamodel.me>
2016-03-14 00:14:10 +0100

    Updated translations from transifex.

--- contao/languages/fr/tl_metamodel_dcasetting.php
+++ contao/languages/fr/tl_metamodel_dcasetting.php
@@ -8,9 +8,10 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T22:13:06+02:00
+ * last-updated: 2015-07-11T04:11:31+02:00
  */
 
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['0']           = 'Afficher en tant que boutons radio';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['1']           = 'Sélectionner cette option pour changer le type d\'affichage de "select" à "radio"';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio_reference']['2'] = 'Afficher comme pop-up de sélection';
 
-$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['0'] = 'Afficher en tant que boutons radio';
-$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['1'] = 'Sélectionner cette option pour changer le type d\'affichage de "select" à "radio"';
