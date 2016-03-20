365c0bed7f70d362a3b2b2b95b11fb41dcbb6207
Mini Model <minimodel@metamodel.me>
2016-03-21 00:17:14 +0100

    Updated translations from transifex.

--- contao/languages/fr/tl_metamodel_dcasetting.php
+++ contao/languages/fr/tl_metamodel_dcasetting.php
@@ -8,10 +8,16 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-11T04:11:31+02:00
+ * last-updated: 2016-03-14T00:14:10+01:00
  */
 
-$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['0']           = 'Afficher en tant que boutons radio';
-$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['1']           = 'Sélectionner cette option pour changer le type d\'affichage de "select" à "radio"';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['0']           = 'Affichage';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio']['1']           = 'Sélectionner l\'affichage souhaité.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio_reference']['0'] = 'Afficher comme menu de sélection.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio_reference']['1'] = 'Afficher comme liste de boutons radio (sélection unique).';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_as_radio_reference']['2'] = 'Afficher comme pop-up de sélection';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_maxLevel']['0']           = 'Niveau maximum dans l\'arborescence';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_maxLevel']['1']           = 'En cas de valeur >0, aucun élément ne sera sélectionnable au delà de ce niveau .';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_minLevel']['0']           = 'Niveau minimum dans l\'arborescence.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['select_minLevel']['1']           = 'En cas de valeur >0, aucun élément ne sera sélectionnable en dessous de ce niveau .';
 
