a68234dd84322f173ebaa7fe3a6de83bb647af96
Mini Model <minimodel@metamodel.me>
2017-07-09 22:02:32 +0200

    Updated translations from transifex.

---
+++ contao/languages/et/tl_metamodel_attribute.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/$$project$$/language/$$lang$$/
+ *
+ * last-updated: 2017-07-09T22:02:25+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_alias']['0']                 = 'Aliase veerg';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_column']['0']                = 'Väärtuse veerg';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_column_type']['sql']         = 'SQL tabeli veerg';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_filter']['0']                = 'Filter';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_filterparams']['0']          = 'Filtri parameetrid';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_id']['0']                    = 'ID veerg';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_sorting']['0']               = 'Vali sorteerimine';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_table_type']['sql-table']    = 'SQL tabel';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_table_type']['translated']   = 'Tõlgitud MetaModels';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_table_type']['untranslated'] = 'Tõlkimata MetaModels';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_where']['0']                 = 'SQL';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['sql_error']                         = 'SQL päring tekitab tõrke.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['select']             = 'Vali';
+
