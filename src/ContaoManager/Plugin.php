73c6828057293a5f25cbe2f2061c5788ecd3b4b7
Christian Schiffler <c.schiffler@cyberspectrum.de>
2017-11-07 02:18:33 +0100

    Remove route loading from plugin.
    We do not have custom entry points in this bundle.

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -13,6 +13,7 @@
  * @package    MetaModels
  * @subpackage AttributeSelect
  * @author     David Molineus <david.molineus@netzmacht.de>
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @copyright  2012-2017 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_text/blob/master/LICENSE LGPL-3.0
  * @filesource
@@ -23,16 +24,13 @@ namespace MetaModels\AttributeSelectBundle\ContaoManager;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use MetaModels\AttributeSelectBundle\MetaModelsAttributeSelectBundle;
 use MetaModels\CoreBundle\MetaModelsCoreBundle;
-use Symfony\Component\Config\Loader\LoaderResolverInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
  * Contao Manager plugin.
  */
-class Plugin implements BundlePluginInterface, RoutingPluginInterface
+class Plugin implements BundlePluginInterface
 {
     /**
      * {@inheritdoc}
@@ -48,15 +46,5 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface
                 )
                 ->setReplace(['metamodelsattribute_select'])
         ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
-    {
-        $resolver
-            ->resolve(__DIR__ . '/../Resources/config/routing.yml')
-            ->load(__DIR__ . '/../Resources/config/routing.yml');
     }
 }
