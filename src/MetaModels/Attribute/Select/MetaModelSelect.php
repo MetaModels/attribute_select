e77a43f55c488dff8ecfc23005ddeae1a8699d95
Christian Schiffler <c.schiffler@cyberspectrum.de>
2017-11-17 04:51:26 +0100

    Optimize getFilterOptionsForDcGeneral()
    We now only fetch the columns we really use.

--- src/MetaModels/Attribute/Select/MetaModelSelect.php
+++ src/MetaModels/Attribute/Select/MetaModelSelect.php
@@ -253,7 +253,14 @@ class MetaModelSelect extends AbstractSelect
 
         $this->buildFilterRulesForFilterSetting($filter);
 
-        $objItems = $this->getSelectMetaModel()->findByFilter($filter, $this->getSortingColumn());
+        $objItems = $this->getSelectMetaModel()->findByFilter(
+            $filter,
+            $this->getSortingColumn(),
+            0,
+            0,
+            'ASC',
+            [$this->getValueColumn(), $this->getIdColumn()]
+        );
 
         $GLOBALS['TL_LANGUAGE'] = $originalLanguage;
 
