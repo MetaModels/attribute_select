95aca1b9ac0f9d2d1a1c1acc9aec7084bb2848a6
Ingolf Steinhardt <info@e-spin.de>
2018-01-05 14:26:09 +0100

    hotfix methode sortIds with Connection::PARAM_STR_ARRAY

--- src/Attribute/Select.php
+++ src/Attribute/Select.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_select.
  *
- * (c) 2012-2017 The MetaModels team.
+ * (c) 2012-2018 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -22,7 +22,7 @@
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2017 The MetaModels team.
+ * @copyright  2012-2018 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_select/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -64,9 +64,9 @@ class Select extends AbstractSelect
             ->leftJoin('m', $strTableName, 's', sprintf('s.%s = m.%s', $strColNameId, $this->getColName()))
             ->where('m.id IN (:ids)')
             ->orderBy('s.' . $strSortColumn, $strDirection)
-            ->setParameter('ids', $idList)
+            ->setParameter('ids', $idList, Connection::PARAM_STR_ARRAY)
             ->execute()
-            ->fetch(\PDO::FETCH_COLUMN);
+            ->fetchAll(\PDO::FETCH_COLUMN);
 
         return $idList;
     }
