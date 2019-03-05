deac6c2468572ee850ab27853626a999eb00e4c1
Mini Model <minimodel@metamodel.me>
2019-03-05 19:37:00 +0100

    Update PHPDoc file headers

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,21 +1,20 @@
 <?php
 
 /**
- * * This file is part of MetaModels/attribute_select.
+ * This file is part of MetaModels/attribute_select.
  *
- * (c) 2012-2017 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage AttributeSelect
+ * @package    MetaModels/attribute_select
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2012-2017 The MetaModels team.
- * @license    https://github.com/MetaModels/attribute_text/blob/master/LICENSE LGPL-3.0
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_select/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
