139a764c3020a54efcfbe541e9a78b0ca54a2b23
Jörn <github2014@diedrichsen.net>
2019-04-14 16:36:25 +0200

    Typos

--- src/Resources/contao/languages/de/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/de/tl_metamodel_attribute.php
@@ -12,9 +12,9 @@
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_alias']['0']                 = 'Alias-Spalte';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_alias']['1']                 = 'Bitte wählen Sie die Tabellenspalte die als Optionsalias (zum Beispiel in Filterwidgets) benutzt werden soll. Falls Sie unsicher sind wählen Sie dieselbe Spalte wie für die ID aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_alias']['1']                 = 'Bitte wählen Sie die Tabellenspalte, die als Optionsalias (zum Beispiel in Filterwidgets) benutzt werden soll. Falls Sie unsicher sind, wählen Sie dieselbe Spalte wie für die ID aus.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_column']['0']                = 'Werte-Spalte';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_column']['1']                = 'Bitte wählen Sie die Tabellenspalte aus der ein Textwert bezogen werden soll.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_column']['1']                = 'Bitte wählen Sie die Tabellenspalte, aus der ein Textwert bezogen werden soll.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_column_type']['attribute']   = 'MetaModels Attribute';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_column_type']['sql']         = 'Spalte der SQL-Tabelle';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_filter']['0']                = 'Filter';
@@ -22,7 +22,7 @@ $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_filter']['1']             
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_filterparams']['0']          = 'Filterparameter';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_filterparams']['1']          = 'Hier können Sie eine Vorgabe für den Filter wählen..';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_id']['0']                    = 'ID-Spalte';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_id']['1']                    = 'Bitte wählen Sie die Tabellenspalte aus die als Datensatz-ID dienen solll.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_id']['1']                    = 'Bitte wählen Sie die Tabellenspalte aus, die als Datensatz-ID dienen solll.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_sorting']['0']               = 'Auswahl-Sortierung';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_sorting']['1']               = 'Bitte wählen Sie einen Eintrag für die Tag-Sortierung aus.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_table']['0']                 = 'Quelltabelle';
